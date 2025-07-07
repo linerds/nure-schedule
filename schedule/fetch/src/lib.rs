f6bab33313f17e2cdc9fbdc591d0e0ca10a9f3cc
Anton Bilous <oxidate@tuta.io>
2025-07-07 22:54:05 +0300

    test: fetch API health

--- schedule/fetch/src/lib.rs
+++ schedule/fetch/src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use chrono::{DateTime, Utc};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Health {
+    uptime: f64,
+    message: String,
+    date: DateTime<Utc>,
+}
+
+impl Health {
+    pub fn fetch() -> Result<Self, Box<dyn std::error::Error>> {
+        Ok(ureq::get("https://sh.mindenit.org/api/health")
+            .call()?
+            .body_mut()
+            .read_json()?)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    // cargo test -- --nocapture
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn health_fetch() -> Result<(), Box<dyn std::error::Error>> {
+        let health = Health::fetch()?;
+
+        println!("Health Status:");
+        println!("\tUptime: {:.2} seconds", health.uptime);
+        println!("\tMessage: {}", health.message);
+        println!("\tDate: {}", health.date);
+
+        Ok(())
     }
 }
